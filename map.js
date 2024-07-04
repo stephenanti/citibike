aa2270d28ff6b20529862a78d0a6dd3bebb116b4
Stephen Anti <sma2263@columbia.edu>
2024-07-04 14:09:22 -0700

    Update map.js

--- map.js
+++ map.js
@@ -1,5 +1,5 @@
 mapboxgl.accessToken =
-"pk.eyJ1Ijoic3RlcGhlbmFudGkiLCJhIjoiY2t4NDN5MDJ2MDJkcTJ2cGFjOGk3MW9jcyJ9.idvR4RaqkBIA5w32u482kw";
+"pk.eyJ1Ijoic3RlcGhlbmFudGkiLCJhIjoiY2x5M2ZndmtqMDM4YTJrb2ozN24zb3JmbCJ9.qSuAh5SE-sou1NQF3xYUGQ";
 var map = new mapboxgl.Map({
   container: "map",
   style: "mapbox://styles/stephenanti/cl4c4x6se000b16my8a3ht9m3",
